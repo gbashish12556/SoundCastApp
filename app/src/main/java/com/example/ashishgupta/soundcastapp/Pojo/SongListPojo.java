208ba388cff077c4c3a57ea0f63ddca98a51a70f
Ashish <gbashish12556@gmail.com>
2018-10-26 10:02:23 +0530

    added pojo class for song list

---
+++ app/src/main/java/com/example/ashishgupta/soundcastapp/Pojo/SongListPojo.java
@@ -0,0 +1,42 @@
+package com.example.ashishgupta.soundcastapp.Pojo;
+
+public class SongListPojo {
+
+    private int songId;
+    private String thumbnNailUrl;
+    private String songName;
+    private String songURL;
+    
+    public int getSongId() {
+        return songId;
+    }
+
+    public void setSongId(int songId) {
+        this.songId = songId;
+    }
+
+    public String getThumbnNailUrl() {
+        return thumbnNailUrl;
+    }
+
+    public void setThumbnNailUrl(String thumbnNailUrl) {
+        this.thumbnNailUrl = thumbnNailUrl;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public String getSongURL() {
+        return songURL;
+    }
+
+    public void setSongURL(String songURL) {
+        this.songURL = songURL;
+    }
+
+}
